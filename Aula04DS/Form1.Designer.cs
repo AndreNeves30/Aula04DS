57a3c14f30d5e01f27b162cd6d5ffa5001c3f985
AndreNeves30 <andre.neves30@etec.sp.gov.br>
2023-03-03 20:20:35 -0300

    Add project files.

---
+++ Aula04DS/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace Aula04DS
+{
+    partial class FormPersonagens
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbNome = new System.Windows.Forms.Label();
+            this.pictureBoxPersonagem = new System.Windows.Forms.PictureBox();
+            this.listBoxPersonagem = new System.Windows.Forms.ListBox();
+            this.comboBoxPersonagem = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPersonagem)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbNome
+            // 
+            this.lbNome.AutoSize = true;
+            this.lbNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNome.ForeColor = System.Drawing.Color.White;
+            this.lbNome.Location = new System.Drawing.Point(207, 426);
+            this.lbNome.Name = "lbNome";
+            this.lbNome.Size = new System.Drawing.Size(300, 15);
+            this.lbNome.TabIndex = 1;
+            this.lbNome.Text = "André de Sousa Neves e Joyce Rufino Pereira";
+            // 
+            // pictureBoxPersonagem
+            // 
+            this.pictureBoxPersonagem.Location = new System.Drawing.Point(217, 33);
+            this.pictureBoxPersonagem.Name = "pictureBoxPersonagem";
+            this.pictureBoxPersonagem.Size = new System.Drawing.Size(290, 345);
+            this.pictureBoxPersonagem.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxPersonagem.TabIndex = 2;
+            this.pictureBoxPersonagem.TabStop = false;
+            // 
+            // listBoxPersonagem
+            // 
+            this.listBoxPersonagem.FormattingEnabled = true;
+            this.listBoxPersonagem.Items.AddRange(new object[] {
+            "Goku",
+            "Vegeta",
+            "Inuyasha",
+            "Sesshoumaru",
+            "Knight",
+            "Sanji",
+            "Kaneki",
+            "Ban",
+            "Violet",
+            "Howl"});
+            this.listBoxPersonagem.Location = new System.Drawing.Point(34, 53);
+            this.listBoxPersonagem.Name = "listBoxPersonagem";
+            this.listBoxPersonagem.Size = new System.Drawing.Size(129, 160);
+            this.listBoxPersonagem.TabIndex = 3;
+            this.listBoxPersonagem.SelectedIndexChanged += new System.EventHandler(this.listBoxPersonagem_SelectedIndexChanged);
+            // 
+            // comboBoxPersonagem
+            // 
+            this.comboBoxPersonagem.FormattingEnabled = true;
+            this.comboBoxPersonagem.Items.AddRange(new object[] {
+            "Goku",
+            "Vegeta",
+            "Inuyasha",
+            "Sesshoumaru",
+            "Knight",
+            "Sanji",
+            "Kaneki",
+            "Ban",
+            "Violet",
+            "Howl"});
+            this.comboBoxPersonagem.Location = new System.Drawing.Point(34, 287);
+            this.comboBoxPersonagem.Name = "comboBoxPersonagem";
+            this.comboBoxPersonagem.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxPersonagem.TabIndex = 4;
+            this.comboBoxPersonagem.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // FormPersonagens
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBoxPersonagem);
+            this.Controls.Add(this.listBoxPersonagem);
+            this.Controls.Add(this.pictureBoxPersonagem);
+            this.Controls.Add(this.lbNome);
+            this.Name = "FormPersonagens";
+            this.Text = "O Programa dos Personagens";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPersonagem)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbNome;
+        private System.Windows.Forms.PictureBox pictureBoxPersonagem;
+        private System.Windows.Forms.ListBox listBoxPersonagem;
+        private System.Windows.Forms.ComboBox comboBoxPersonagem;
+    }
+}
+
---
+++ Aula04DS/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula04DS
+{
+    public partial class FormPersonagens : Form
+    {
+        public FormPersonagens()
+        {
+            InitializeComponent();
+        }
+        private void mudarPersonagem()
+        {
+            if (comboBoxPersonagem.SelectedIndex == 0 || listBoxPersonagem.SelectedIndex == 0)
+            {
+                pictureBoxPersonagem.Image = Properties.Resources.goku;
+                this.BackColor = Color.Orange;
+            }
+            else if (comboBoxPersonagem.SelectedIndex == 1 || listBoxPersonagem.SelectedIndex == 1)
+            {
+                pictureBoxPersonagem.Image = Properties.Resources.vegeta;
+                this.BackColor = Color.Blue;
+            }
+            else if(comboBoxPersonagem.SelectedIndex == 2 || listBoxPersonagem.SelectedIndex == 2)
+            {
+                pictureBoxPersonagem.Image = Properties.Resources.inuyasha;
+                this.BackColor = Color.Red;
+            }
+            else if(comboBoxPersonagem.SelectedIndex == 3 || listBoxPersonagem.SelectedIndex == 3)
+            {
+                pictureBoxPersonagem.Image = Properties.Resources.sesshoumaru;
+                this.BackColor = Color.Gray;
+            }
+            else if(comboBoxPersonagem.SelectedIndex == 4 || listBoxPersonagem.SelectedIndex == 4)
+            {
+                pictureBoxPersonagem.Image = Properties.Resources.knight;
+                this.BackColor = Color.Black;
+            }
+            else if(comboBoxPersonagem.SelectedIndex == 5 || listBoxPersonagem.SelectedIndex == 5)
+            {
+                pictureBoxPersonagem.Image = Properties.Resources.sanji;
+                this.BackColor = Color.Purple;
+            }
+            else if(comboBoxPersonagem.SelectedIndex == 6 || listBoxPersonagem.SelectedIndex == 6)
+            {
+                pictureBoxPersonagem.Image = Properties.Resources.kaneki;
+                this.BackColor = Color.IndianRed;
+            }
+            else if(comboBoxPersonagem.SelectedIndex == 7 || listBoxPersonagem.SelectedIndex == 7)
+            {
+                pictureBoxPersonagem.Image = Properties.Resources.ban;
+                this.BackColor = Color.DarkRed;
+            }
+            else if(comboBoxPersonagem.SelectedIndex == 8 || listBoxPersonagem.SelectedIndex == 8)
+            {
+                pictureBoxPersonagem.Image = Properties.Resources.violet;
+                this.BackColor = Color.Violet;
+            }
+            else if(comboBoxPersonagem.SelectedIndex == 9 || listBoxPersonagem.SelectedIndex == 9)
+            {
+                pictureBoxPersonagem.Image = Properties.Resources.howl;
+                this.BackColor = Color.MediumPurple;
+            }
+           
+           
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            mudarPersonagem();
+        }
+
+        private void listBoxPersonagem_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            mudarPersonagem();
+        }
+    }
+}
